f62e76e714030af77ebd2e56d381d35c8b6a159e
Ryan Herbst <rherbst@slac.stanford.edu>
2022-12-17 22:20:49 -0800

    Updates

--- rrCrossingControl.py
+++ rrCrossingControl.py
@@ -6,47 +6,61 @@ import time
 import threading
 import serial
 
+SensorStates = {'0': 'false',
+                '1': 'true'}
+
+SignalStates = {'0': 'green',
+                '1': 'red'}
 
 class RrCrossing(object):
 
 
     def __init__(self):
-        self._sensors = [False] * 4
+        self._sensors = ['unknown'] * 4
+        self._rrCross = 'unknown'
         self._runEnable = False
         self._serialThread = threading.Thread(target=self._runSerial)
         self._mqttThread = threading.Thread(target=self._runMqtt)
-        self._contThread = threading.Thread(target=self._runControl)
         self._mqttQueue = queue.SimpleQueue()
-        self._servo = [90] * 2
-        self._led = 0
 
 
     def start(self):
         self._runEnable = True
-        self._contThread.start()
         self._serialThread.start()
         self._mqttThread.start()
-        self._mqttQueue.put(True)
 
 
     def stop(self):
         self._runEnable = False
         self._serialThread.join()
         self._mqttThread.join()
-        self._contThread.join()
 
 
-    def _rxSensors(self, values):
-        update = False
+    def _runSerial(self):
 
-        for x in range(len(values)):
+        while self._runEnable:
+            try:
+                with serial.Serial("/dev/ttyACM0", 9600) as s:
 
-            if self._sensors[x] != values[x]:
-                self._sensors[x] = values[x]
-                update = True
+                    while self._runEnable:
+                        line = s.readline().decode('UTF-8')
+                        fields = line.rstrip().split(' ')
 
-        if update:
-            self._mqttQueue.put(True)
+                        if len(fields) == 6 and fields[0] == 'Status':
+                            for x in range(4):
+
+                                if self._sensors[x] != SensorStates[fields[x+1]]:
+                                    self._sensors[x] = SensorStates[fields[x+1]]
+                                    self._mqttQueue.put(f'<fb id="Sense {x+1}" state="{self._sensors[x]}"/>')
+
+                            if self._rrCross != SignalStates[fields[5]]:
+                                self._rrCross = SignalStates[fields[5]]
+                                self._mqttQueue.put(f'<sg id="Cross" cmd="{self._rrCross}"/>')
+
+                            print(f"Got Data: Sensors: {self._sensors}, Cross: {self._rrCross}")
+
+            except Exception as e:
+                print(f"Serial Error: {e}")
 
 
     def _runMqtt(self):
@@ -65,43 +79,11 @@ class RrCrossing(object):
                 work = self._mqttQueue.get_nowait()
 
                 if work:
-                    for x in range(len(self._sensors)):
-                        client.publish(f"rr_cross/status/sense{x}", self._sensors[x])
+                    print(f"Sending: {work}")
+                    client.publish(f"rocrail/service/client", work)
 
             client.loop()
             time.sleep(.1)
-
-
-    def _runSerial(self):
-
-        while self._runEnable:
-            try:
-                with serial.Serial("/dev/ttyACM0", 9600) as s:
-
-                    while self._runEnable:
-                        line = s.readline().decode('UTF-8')
-                        fields = line.rstrip().split(' ')
-
-                        if len(fields) == 5 and fields[0] == 'Status':
-                            values = [True if fields[x] != "0" else False for x in range(1,len(fields))]
-                            self._rxSensors(values)
-
-                        s.write(f"set {self._led} {self._servo[0]} {self._servo[1]}".encode('UTF-8'))
-
-            except Exception as e:
-                print(f"Serial Error: {e}")
-
-
-    def _runControl(self):
-
-        while self._runEnable:
-
-
-            time.sleep(.1)
-
-
-
-
 
 
 rr = RrCrossing()
--- rr_crossing_device/rr_crossing_device.ino
+++ rr_crossing_device/rr_crossing_device.ino
@@ -5,27 +5,38 @@ Servo ServoA;
 Servo ServoB;
 
 const unsigned int SensorCnt  = 4;
-const unsigned int ServoCnt = 2;
-const unsigned int LedCnt   = 4;
 const unsigned int SensorPin[SensorCnt] = {8, 11, 12, 13};
-const unsigned int ServoPin[ServoCnt] = {9, 10};
+const unsigned long SensorOffPeriod = 500;
+const unsigned long SensorOnPeriod = 100;
+
+const unsigned long SendPeriod = 1000;
+const unsigned long CrossPeriod = 3000;
+
+const unsigned int LedCnt   = 4;
+const unsigned long LedPeriod = 250;
 const unsigned int LedPin[LedCnt] = {4, 5, 6, 7};
 
-unsigned int x;
-unsigned int servoValue[ServoCnt];
-unsigned int ledValue;
-unsigned int sensorValue[SensorCnt];
-unsigned int newValue[SensorCnt];
-unsigned int send;
-unsigned int rxCount;
-unsigned int ledSet;
-unsigned int servoSet[ServoCnt];
-unsigned long lastSend;
+const unsigned int ServoCnt = 2;
+const unsigned int ServoPin[ServoCnt] = {9, 10};
+const unsigned int ServoUp[ServoCnt] = {120,120};
+const unsigned int ServoDown[ServoCnt] = {85,82};
 
-int ret;
-char c;
-char mark[20];
-char rxBuffer[50];
+unsigned int x;
+unsigned long currTime;
+
+unsigned int sensorRaw[SensorCnt];
+unsigned int sensorValue[SensorCnt];
+unsigned long sensorTime[SensorCnt];
+unsigned int sensorsOn;
+
+unsigned int crossState;
+unsigned long crossTime;
+
+unsigned int ledState;
+unsigned long ledTime;
+
+unsigned int doSend;
+unsigned long lastSend;
 char txBuffer[50];
 
 Servo rrServo[ServoCnt];
@@ -35,88 +46,142 @@ void setup() {
 
    for(x=0; x < ServoCnt; x++) {
       rrServo[x].attach(ServoPin[x]);
-      servoValue[x] = 90;
-      rrServo[x].write(servoValue[x]);
+      rrServo[x].write(ServoUp[x]);
    }
 
    for(x=0; x < LedCnt; x++) {
       pinMode(LedPin[x], OUTPUT);
       digitalWrite(LedPin[x], HIGH);
    }
-   ledValue = 0;
+
+   ledState = 0;
+   ledTime = millis();
+   crossState = 0;
+   crossTime = millis();
 
    for(x=0; x < SensorCnt; x++) {
       pinMode(SensorPin[x], INPUT_PULLUP);
+      sensorTime[x] = millis();
       sensorValue[x] = 0;
-      newValue[x] = 0;
+      sensorRaw[x] = 0;
    }
-
-   rxCount = 0;
+   sensorsOn = 0;
+   lastSend = millis();
 }
 
 void loop(){
+   doSend = 0;
+   currTime = millis();
 
-   for(x=0; x < ServoCnt; x++) {
-      if (servoValue[x] > 180) servoValue[x] = 180;
-      rrServo[x].write(servoValue[x]);
-   }
-
-   if ( ledValue == 1 ) {
-      for (x=0; x < LedCnt; x += 2) {
-         digitalWrite(LedPin[x], HIGH);
-         digitalWrite(LedPin[x+1], LOW);
-      }
-   }
-
-   else if ( ledValue == 2 ) {
-      for (x=0; x < LedCnt; x += 2) {
-         digitalWrite(LedPin[x], LOW);
-         digitalWrite(LedPin[x+1], HIGH);
-      }
-   }
-
-   else {
-      for (x=0; x < LedCnt; x++) digitalWrite(LedPin[x], HIGH);
-   }
-
-   send = 0;
+   //////////////////////////////////
+   // Update sensor values
+   //////////////////////////////////
+   sensorsOn = 0;
 
    for (x=0; x < SensorCnt; x++) {
-      if (digitalRead(SensorPin[x]) == 0) newValue[x] = 1;
-      else newValue[x] = 0;
 
-      if (newValue[x] != sensorValue[x]) send = 1;
-      sensorValue[x] = newValue[x];
-   }
-
-   if ( (millis() - lastSend) > 200 ) send = 1;
-
-   if (send) {
-      sprintf(txBuffer,"Status %i %i %i %i\n",sensorValue[0],sensorValue[1],sensorValue[2],sensorValue[3]);
-      Serial.write(txBuffer);
-      lastSend = millis();
-   }
-
-   // Get serial data
-   while (Serial.available()) {
-      if ( rxCount == 49 ) rxCount = 0;
-
-      c = Serial.read();
-      rxBuffer[rxCount++] = c;
-      rxBuffer[rxCount] = '\0';
-   }
-
-   // Check for incoming message
-   if ( rxCount > 6 && rxBuffer[rxCount-1] == '\n') {
-
-      // Parse string
-      ret = sscanf(rxBuffer,"%s %i %i %i", mark, &ledSet, &(servoSet[0]), &(servoSet[1]));
-
-      if (ret == 4 && strcmp(mark,"set")) {
-         ledValue = ledSet;
-         for(x=0; x < ServoCnt; x++) servoValue[x] = servoSet[x];
+      // Reset time anytime the sensor is on
+      if (digitalRead(SensorPin[x]) == 0) {
+         if (sensorRaw[x] == 0) sensorTime[x] = currTime;
+         sensorRaw[x] = 1;
       }
-      rxCount = 0;
+      else {
+         if (sensorRaw[x] == 1) sensorTime[x] = currTime;
+         sensorRaw[x] = 0;
+      }
+
+      // Change
+      if (sensorRaw[x] != sensorValue[x]) {
+
+         // Turning On
+         if ( sensorRaw[x] == 1 && ( (currTime - sensorTime[x]) > SensorOnPeriod ) ) {
+            doSend = 1;
+            sensorValue[x] = 1;
+         }
+
+         // Turning off after hold time
+         else if ( sensorRaw[x] == 0 && ( (currTime - sensorTime[x]) > SensorOffPeriod ) ) {
+            doSend = 1;
+            sensorValue[x] = 0;
+         }
+      }
+
+      if ( sensorValue[x] ) sensorsOn = 1;
    }
+
+   //////////////////////////////////
+   // Update Crossing State
+   //////////////////////////////////
+
+   // Update cross on timestamp
+   if ( sensorsOn ) crossTime = currTime;
+
+   // Sensor state does not match cross state
+   if ( sensorsOn != crossState ) {
+
+      // Turning on
+      if ( sensorsOn ) {
+         crossState = 1;
+         doSend = 1;
+      }
+
+      // Turning off after hold time
+      else if ( (currTime - crossTime) > CrossPeriod ) {
+         crossState = 0;
+         doSend = 1;
+      }
+   }
+
+   //////////////////////////////////
+   // Update servo Values
+   //////////////////////////////////
+   for(x=0; x < ServoCnt; x++) {
+      if (crossState) rrServo[x].write(ServoDown[x]);
+      else rrServo[x].write(ServoUp[x]);
+   }
+
+   //////////////////////////////////
+   // Update LED State
+   //////////////////////////////////
+
+   // LEDs On
+   if ( crossState ) {
+
+      // Transition
+      if ( (currTime - ledTime) > LedPeriod )  {
+         ledTime = currTime;
+
+         if ( ledState ) {
+            for(x=0; x < LedCnt; x += 2) {
+               digitalWrite(LedPin[x], HIGH);
+               digitalWrite(LedPin[x+1], LOW);
+            }
+            ledState = 0;
+         }
+
+         else {
+            for(x=0; x < LedCnt; x += 2) {
+               digitalWrite(LedPin[x], LOW);
+               digitalWrite(LedPin[x+1], HIGH);
+            }
+            ledState = 1;
+         }
+      }
+   }
+
+   // LEDs Off
+   else {
+      for (x=0; x < LedCnt; x++) digitalWrite(LedPin[x], HIGH);
+      ledTime = currTime;
+   }
+
+   if ( (currTime - lastSend) > SendPeriod ) doSend = 1;
+
+   if (doSend) {
+      sprintf(txBuffer,"Status %i %i %i %i %i\n",sensorValue[0],sensorValue[1],sensorValue[2],sensorValue[3],crossState);
+      Serial.write(txBuffer);
+      lastSend = currTime;
+   }
+
 }
 
